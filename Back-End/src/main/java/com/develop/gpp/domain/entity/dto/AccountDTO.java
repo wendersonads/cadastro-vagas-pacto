ca13a547b4445cb5afef39d3c08efee86792a65f
wendersonads <wendersonads5.5@gmail.com>
2024-08-29 11:25:48 -0300

    :recycle: Refatorando Account DTO e Vaga DTO

--- Back-End/src/main/java/com/develop/gpp/domain/entity/dto/AccountDTO.java
+++ Back-End/src/main/java/com/develop/gpp/domain/entity/dto/AccountDTO.java
@@ -11,17 +11,11 @@ public class AccountDTO {
     private Long idPerfilUsuario;
     private String descricaoPerfil;
     private List<VagaDTO> vagas;
-    private Integer idVaga;
-    private String descricaoVaga;
-    private String tituloVaga;
 
     public AccountDTO(String nomeUsuario, Long idPerfilUsuario, String descricaoPerfil, List<VagaDTO> vagas) {
         this.nomeUsuario = nomeUsuario;
         this.idPerfilUsuario = idPerfilUsuario;
         this.descricaoPerfil = descricaoPerfil;
         this.vagas = vagas;
-        // this.idVaga = idVaga;
-        // this.descricaoVaga = descricaoVaga;
-        // this.tituloVaga = tituloVaga;
     }
 }
